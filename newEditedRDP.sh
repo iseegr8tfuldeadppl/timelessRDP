522cf5884fe606fc2f96085a126a010909839563
kream <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2020-12-31 13:30:33 +0100

    a

--- newEditedRDP.sh
+++ newEditedRDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m ISEEGR8TFULDEADPPL
-sudo adduser ISEEGR8TFULDEADPPL sudo
-echo 'ISEEGR8TFULDEADPPL:1798' | sudo chpasswd
+sudo useradd -m YESTHEPOTATOISLOOKINGATYOU
+sudo adduser YESTHEPOTATOISLOOKINGATYOU sudo
+echo 'YESTHEPOTATOISLOOKINGATYOU:1798' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser ISEEGR8TFULDEADPPL chrome-remote-desktop
+sudo adduser YESTHEPOTATOISLOOKINGATYOU chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ISEEGR8TFULDEADPPL -c """$CRP"""
+su - YESTHEPOTATOISLOOKINGATYOU -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
